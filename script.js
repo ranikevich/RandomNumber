938ea30edcf2c2f1d27b89ae036ed859fe9009f8
Rimma Anikevich <ranikevich@gmail.com>
2022-04-08 18:23:12 +0700

    Add analyze number

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+'use strict';
 
+
+function guessNumber(x) {
+    function askAnalyzeNumber() {
+        let a = prompt("Угадай число от 1 до 100:");
+        if (a !== null) {   
+        a = +a;
+        if (x != a) {
+            (a != parseInt(a)) ? a = confirm('Введи число!'):
+            (x < a) ? a = confirm('Загаданное число меньше') :
+            (x > a) ? a = confirm('Загаданное число больше') : null;
+            askAnalyzeNumber();
+        }
+        return alert("Поздравляю, Вы угадали!!!");  
+    } 
+    return alert("Игра окончена");
+}
+askAnalyzeNumber();
+}
+
+guessNumber(72);
