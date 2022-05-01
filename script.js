0f9ea234e99db027f6b94b5276caa67e241b98c4
Rimma Anikevich <ranikevich@gmail.com>
2022-05-02 00:37:58 +0700

    Add attempt

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 'use strict';
 
 let randomNum = parseInt(Math.random() * 100);
+const attempt = 10;
 
-function guessNumber(x) {
+function guessNumber(num, count) {
     function askAnalyzeNumber() {
-        let a = prompt("Угадай число от 1 до 100:");
-        if (a !== null) {   
-        a = +a;
-        if (x != a) {
-            (a != parseInt(a)) ? a = confirm('Введи число!'):
-            (x < a) ? a = confirm('Загаданное число меньше') :
-            (x > a) ? a = confirm('Загаданное число больше') : null;
-            askAnalyzeNumber();
+        let a = '';
+        if (count > 0) {
+            count = count - 1;
+            a = prompt("Угадай число от 1 до 100:");
+            if (a !== null) {
+                a = +a;
+                if (num != a) {
+                    (a != parseInt(a)) ? a = alert('Введи число!'):
+                    (num < a) ? a = alert('Загаданное число меньше, осталось попыток: ' + count) :
+                    (num > a) ? a = alert('Загаданное число больше, осталось попыток: ' + count) : null;
+                    askAnalyzeNumber();
+                } else {
+                    (num == a) ? a = confirm("Поздравляю!!! Вы угадали. Хотели бы сыграть еще?") : null;
+                    if (a == true) {
+                        randomNum = parseInt(Math.random() * 100);
+                        guessNumber(randomNum, attempt);
+                    }
+                }
+            } else {
+                a = alert("Игра окончена");
+            }
         } else {
-            alert("Поздравляю!!! Вы угадали");
+            a = confirm("Попытки закончились, хотите сыграть еще?");
+            if (a == true) {
+                randomNum = parseInt(Math.random() * 100);
+                guessNumber(randomNum, attempt);
+            }
         }
-    } else {
-        alert("Игра окончена");
     }
-}
 askAnalyzeNumber();
 }
 
-guessNumber(randomNum);
+guessNumber(randomNum, attempt);
